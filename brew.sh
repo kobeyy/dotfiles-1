0b05134fd8c42c9c5046d547096da48c20947348
Kobe Van Reusel <vanreuse@Kobes-MacBook-Pro.local>
2021-03-01 13:47:57 +0100

    change mac pref, add brew casks

--- brew.sh
+++ brew.sh
@@ -25,6 +25,8 @@ brew install gnu-sed --with-default-names
 # Install a modern version of Bash.
 brew install bash
 brew install bash-completion2
+brew install zsh zsh-syntax-highlighting zsh-autosuggestions
+brew install jq node postgres
 
 # Switch to using brew-installed bash as default shell
 if ! fgrep -q "${BREW_PREFIX}/bin/bash" /etc/shells; then
@@ -54,29 +56,9 @@ brew install woff2
 
 # Install some CTF tools; see https://github.com/ctfs/write-ups.
 brew install aircrack-ng
-brew install bfg
-brew install binutils
-brew install binwalk
-brew install cifer
-brew install dex2jar
-brew install dns2tcp
-brew install fcrackzip
-brew install foremost
-brew install hashpump
-brew install hydra
-brew install john
-brew install knock
-brew install netpbm
 brew install nmap
-brew install pngcheck
-brew install socat
-brew install sqlmap
-brew install tcpflow
-brew install tcpreplay
 brew install tcptrace
-brew install ucspi-tcp # `tcpserver` etc.
 brew install xpdf
-brew install xz
 
 # Install other useful binaries.
 brew install ack
@@ -99,3 +81,7 @@ brew install zopfli
 
 # Remove outdated versions from the cellar.
 brew cleanup
+
+# casks
+brew install --cask visual-studio-code
+brew install --cask intellij-idea
